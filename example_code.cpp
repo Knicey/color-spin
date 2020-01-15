385878f62ebd20c84aba8c25c34031f1e66f2044
Nathan Yang <nathan.s.yang@gmail.com>
2020-01-15 15:58:12 -0800

    added single quotes

--- example_code.cpp
+++ example_code.cpp
@@ -5,7 +5,7 @@ int main()
   char color;
   char color2;
   float rotate;
-  
+  using namespace std;
   //This is the actual code that would be used, it's commented currently to test the rest of the function
   //color = camera_input
   //color2 = player_input
@@ -15,39 +15,39 @@ int main()
   cin >> color2;
   
   //The first if-elseif statement can only check for 3 different conditions, that is why the last operation leads into an if-else
-  if ( color == r ) {
-    if ( color2 == y ) {
-    rotate = .125
+  if ( color == 'r' ) {
+    if ( color2 == 'y' ) {
+    rotate = .125;
     }
-    else if ( color2 == c ) {
-        rotate = .25 )
-        rotate = -.125
+    else if ( color2 == 'c' ) {
+        rotate = .25;
+        rotate = -.125;
     }	
   }
-  else if ( color == y ) {
-    if ( color2 == r ) {
-      rotate = -.125
+  else if ( color == 'y' ) {
+    if ( color2 == 'r' ) {
+      rotate = -.125;
     }
-    else if ( color2 == c ) {
-      rotate = .125
-      rotate = .25
+    else if ( color2 == 'c' ) {
+      rotate = .125;
+      rotate = .25;
     }
-    if ( color == c ) {
-      if ( color2 == r ) {
-        rotate = .25
+    if ( color == 'c' ) {
+      if ( color2 == 'r' ) {
+        rotate = .25;
       }
-      else if ( color2 == y ) {
-        rotate = -.125
-        rotate = .125
+      else if ( color2 == 'y' ) {
+        rotate = -.125;
+        rotate = .125;
       }
     }
     else {
-      if ( color2 == r ) {
-        rotate = .125
+      if ( color2 == 'r' ) {
+        rotate = .125;
       }
-      else if ( color2 == y ) {
-        rotate = .25
-        rotate = -.125
+      else if ( color2 == 'y' ) {
+        rotate = .25;
+        rotate = -.125;
       }
     }
   }
